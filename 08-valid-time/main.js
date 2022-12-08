1e63ec4fc5f48b093fdde36c5d69a4087e9652de
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2022-12-08 14:13:09 +0530

    Day 8 Challenge

---
+++ 08-valid-time/main.js
@@ -0,0 +1,54 @@
+function validTime(str) {
+    //  write code here.
+    const [hour, minute] = str.split(":");
+    return parseInt(hour) <= 24 && parseInt(minute) <= 60;
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
